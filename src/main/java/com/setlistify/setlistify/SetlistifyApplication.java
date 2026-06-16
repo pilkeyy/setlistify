d3dbc6f885a575208f0acc1b5161d3913e81f867
Carlos Garcia <124859617+pilkeyy@users.noreply.github.com.>
2026-06-16 19:21:06 -0400

    initial commit

---
+++ src/main/java/com/setlistify/setlistify/SetlistifyApplication.java
@@ -0,0 +1,13 @@
+package com.setlistify.setlistify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetlistifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetlistifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/setlistify/setlistify/SetlistifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setlistify.setlistify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetlistifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
