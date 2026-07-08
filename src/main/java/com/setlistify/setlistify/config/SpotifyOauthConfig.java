59b212e855d8d5dc3efb3b3eca46f4be9ff8d11e
Carlos Garcia <124859617+pilkeyy@users.noreply.github.com.>
2026-07-08 02:51:01 -0400

    feat: added configurations

--- src/main/java/com/setlistify/setlistify/config/SpotifyOauthConfig.java
+++ src/main/java/com/setlistify/setlistify/config/SpotifyOauthConfig.java
@@ -1,4 +1,26 @@
 package com.setlistify.setlistify.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SpotifyOauthConfig {
+    @Value("${spotify.client-id}")
+    private String clientId;
+    @Value("${spotify.redirect-uri}")
+    private String redirectUri;
+    @Value("${spotify.scopes}")
+    private String scopes;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getRedirectUri() {
+        return redirectUri;
+    }
+
+    public String getScopes() {
+        return scopes;
+    }
 }
